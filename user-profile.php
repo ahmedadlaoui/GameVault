7e01801f7d2bbea9847db4510023a8bfda5c9700
MOHAMED IDRISSI ALAMI <mohamedidrissialami3@gmail.com>
2025-01-03 16:45:20 +0100

    create user profile page

---
+++ user-profile.php
@@ -0,0 +1,119 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>user|profile</title>
+  <link rel="stylesheet" href="user-profile.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+</head>
+
+<body>
+  <div class="menu">
+    <ul>
+      <li class="profile">
+        <div class="img-box">
+          <img src="img/dash-board-Gamal.webp" alt="profile">
+        </div>
+        <h2>nick-name</h2>
+      </li>
+      <li class="active">
+        <a href="#">
+          <i class="fas fa-person"></i>
+          <p>profile</p>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fas fa-gamepad"></i>
+          <p>my library</p>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fas fa-wand-magic-sparkles"></i>
+          <p>list wishes</p>
+        </a>
+      </li>
+      <li>
+        <a href="#">
+          <i class="fas fa-clock"></i>
+          <p>recent games</p>
+        </a>
+      </li>
+
+
+      <li class="log-out">
+        <a href="#">
+          <i class="fas fa-sign-out"></i>
+          <p>log-out</p>
+        </a>
+      </li>
+
+
+
+
+
+    </ul>
+  </div>
+
+  <div class="content">
+    <div class="title-info">
+      <p>statist</p>
+      <i class="fas fa-chart-pie"></i>
+    </div>
+
+    <div class="data-info">
+      <div class="box">
+        <i class="fas fa-coins"></i>
+        <div class="data">
+          <p>score</p>
+          <span>180</span>
+        </div>
+      </div>
+
+      <div class="box">
+        <i class="fas fa-trophy"></i>
+        <div class="data">
+          <p>record</p>
+          <span>280</span>
+        </div>
+      </div>
+
+      <div class="box">
+        <i class="fas fa-user-group"></i>
+        <div class="data">
+          <p>friends</p>
+          <span>380</span>
+        </div>
+      </div>
+
+      <div class="box">
+        <i class="fas fa-crown"></i>
+        <div class="data">
+          <p>Global rank</p>
+          <span>480</span>
+        </div>
+      </div>
+
+
+
+
+    </div>
+
+    <div class="title-info">
+      <p>library</p>
+      <i class="fas fa-table"></i>
+    </div>
+
+
+  </div>
+
+</body>
+</html>
