acee6a6847d64a72f28c245e66f0a2f1e602b807
MOHAMED IDRISSI ALAMI <mohamedidrissialami3@gmail.com>
2025-01-05 21:15:50 +0100

    create a resposive library page 😁😎

---
+++ library.php
@@ -0,0 +1,157 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>library</title>
+  <link rel="stylesheet"type="text/css"  href="library.css">
+</head>
+<body>
+  <header>
+    <ul>
+      <li><a href="#">profile</a></li>
+      <li class="active"><a href="#">library</a></li>
+      <li><a href="#">recent games</a></li>
+      <li><a href="#">wish list</a></li>
+    </ul>
+  </header>
+
+
+
+  <div>
+  <div class="container" >
+    <div class="slider">
+      <div class="slides" style="--img:url('library-img/1.jpg')">
+        <div class="content">
+          <h2>slide 01</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+             Aspernatur, incidunt voluptatem exercitationem recusandae placeat 
+             expedita.</p>
+        </div>
+      </div>
+
+      <div class="slides" style="--img:url('library-img/2.jpg')">
+        <div class="content">
+          <h2>slide 02</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+             Aspernatur, incidunt voluptatem exercitationem recusandae placeat 
+             expedita.</p>
+
+        </div>
+      </div>
+
+      <div class="slides" style="--img:url('library-img/3.jpg')">
+        <div class="content">
+          <h2>slide 03</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+             Aspernatur, incidunt voluptatem exercitationem recusandae placeat 
+             expedita.</p>
+
+        </div>
+      </div>
+
+      <div class="slides" style="--img:url('library-img/4.jpg')">
+        <div class="content">
+          <h2>slide 04</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+             Aspernatur, incidunt voluptatem exercitationem recusandae placeat 
+             expedita.</p>
+
+        </div>
+      </div>
+      <div class="slides" style="--img:url('library-img/5.jpg')">
+        <div class="content">
+          <h2>slide 05</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+             Aspernatur, incidunt voluptatem exercitationem recusandae placeat 
+             expedita.</p>
+
+        </div>
+      </div>
+      <div class="slides" style="--img:url('library-img/6.jpg')">
+        <div class="content">
+          <h2>slide 06</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+             Aspernatur, incidunt voluptatem exercitationem recusandae placeat 
+             expedita.</p>
+
+        </div>
+      </div>
+      <div class="slides" style="--img:url('library-img/7.jpg')">
+        <div class="content">
+          <h2>slide 07</h2>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+             Aspernatur, incidunt voluptatem exercitationem recusandae placeat 
+             expedita.</p>
+
+        </div>
+      </div>
+
+
+
+
+
+    </div>
+    <div class="buttons">
+      <span class="prev"></span>
+      <span class="next"></span>
+    </div>
+  </div>
+</div>
+
+<div class="delim">
+  <div class="line"></div>
+  <div class="expression">
+    <p>Lorem ipsum dolor sit amet consectetur.</p>
+  </div>
+  <div class="line"></div>
+
+</div>
+
+
+  <div class="container2" >
+    <div class="content2">
+      <div class="card">
+        <h2>os</h2>
+        <p>jhvksfh;asdhds</p>
+      </div>
+
+      <div class="card">
+        <h2>os</h2>
+        <p>jhvksfh;asdhds</p>
+      </div>
+      <div class="card">
+        <h2>os</h2>
+        <p>jhvksfh;asdhds</p>
+      </div>
+      <div class="card">
+        <h2>os</h2>
+        <p>jhvksfh;asdhds</p>
+      </div>
+      <div class="card">
+        <h2>os</h2>
+        <p>jhvksfh;asdhds</p>
+      </div>
+      <div class="card">
+        <h2>os</h2>
+        <p>jhvksfh;asdhds</p>
+      </div>
+
+        <h2>os</h2>
+        <p>jhvksfh;asdhds</p>
+      </div>
+
+
+    </div>
+  </div>
+
+
+
+  <script src="library.js"></script>
+</body>
+</html>
